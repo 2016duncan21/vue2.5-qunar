b54e3daea3712cc3676c1ffcb6184874a4233c38
duncan <2016duncan21@gmail.com>
2018-05-21 15:48:47 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img
+        class="banner-img"
+        src="http://img1.qunarzz.com/sight/p0/1412/be/c788f0f3767d2ea037b9b66d2010bfa4.water.jpg_600x330_86333333.jpg">
+      <div class="banner-info">
+        <div class="banner-title">宋城千古情</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe692;</span>
+          22
+        </div>
+      </div>
+    </div>
+    <common-gallary
+      :imgs="imgs"
+      v-show="showGallary"
+      @close="handleGallaryClose"
+    >
+    </common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: [
+        'http://img1.qunarzz.com/sight/p0/1712/fb/fb4ceffb6db67ff7a3.img.jpg_r_800x800_6c0e1d33.jpg',
+        'http://img1.qunarzz.com/sight/p0/1712/17/17d5e111e3707ceea3.img.jpg_r_800x800_87c263fd.jpg'
+      ]
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+<style lang="stylus" scoped type="text/stylus">
+.banner
+  position: relative
+  overflow: hidden
+  height: 0
+  padding-bottom: 55%
+  .banner-img
+    width: 100%
+  .banner-info
+    display: flex
+    position: absolute
+    left: .2rem
+    right: 0
+    bottom: 0
+    line-height: .6rem
+    color: #fff
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0),
+    rgba(0, 0, 0, 0.8))
+    .banner-title
+      flex: 1
+      font-size: .32rem
+      padding: 0 .2rem
+    .banner-number
+      height: .32rem
+      font-size: .24rem
+      margin-top: .14rem
+      padding: 0 .4rem
+      line-height: .32rem
+      border-radius: .2rem
+      background: rgba(0, 0, 0, .8)
+      .banner-icon
+        font-size: .24rem
+</style>
